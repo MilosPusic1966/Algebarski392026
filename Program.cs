74478f3b5de0f8a23b2dd5f7bc4497782ebc80e6
milos pusic <milos.pusic@prvabeogim.edu.rs>
2026-05-25 11:10:51 +0200

    ne valja

--- Program.cs
+++ Program.cs
@@ -8,9 +8,49 @@ namespace Algebarski392026
 {
     internal class Program
     {
+        static int[] p;
+        static int st;
+        static public int nadji(int x)
+        {
+            for (int i = 0; i<st; i++)
+            {
+                if (p[i] == x) return i;
+            }
+            return -1;
+        }
         static void Main(string[] args)
         {
-
+            int n = Convert.ToInt32(Console.ReadLine());
+            p = new int[50];
+            int[] s = new int[50];
+            st = -1;
+            int k = 2;
+            while (n > 1)
+            {
+                Console.WriteLine( "n={0}",n);
+                if ((n % k) == 0)
+                {
+                    int poz = nadji(k);
+                    if (poz > -1)
+                    {
+                        s[poz]++;
+                        Console.WriteLine("s={0}  poz={1}", s, poz);
+                    }
+                    else
+                    {
+                        st++;
+                        p[st] = k;
+                        s[st] = 1;
+                        Console.WriteLine("s={0}  st={1}", s, st);
+                    }
+                    n = n / k;
+                }
+                else k++;
+            }
+            for (int i = 0; i < st; i++)
+            {
+                Console.WriteLine("{0} {1}", p[i], s[i]);
+            }
         }
     }
 }
