49359efbbfbcfdfbe9385c6730adbc533453a42d
milos pusic <milos.pusic@prvabeogim.edu.rs>
2026-05-25 11:18:04 +0200

    dobro

--- Program.cs
+++ Program.cs
@@ -23,25 +23,25 @@ namespace Algebarski392026
             int n = Convert.ToInt32(Console.ReadLine());
             p = new int[50];
             int[] s = new int[50];
-            st = -1;
+            st = 0;
             int k = 2;
             while (n > 1)
             {
-                Console.WriteLine( "n={0}",n);
                 if ((n % k) == 0)
                 {
                     int poz = nadji(k);
+                    
                     if (poz > -1)
                     {
                         s[poz]++;
-                        Console.WriteLine("s={0}  poz={1}", s, poz);
+
                     }
                     else
                     {
-                        st++;
                         p[st] = k;
                         s[st] = 1;
-                        Console.WriteLine("s={0}  st={1}", s, st);
+                        st++;
+                        
                     }
                     n = n / k;
                 }
